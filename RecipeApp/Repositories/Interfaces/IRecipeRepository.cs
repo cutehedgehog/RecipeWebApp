35e5c00153622206dce18841bb80391f7b16acf9
Ksenia Antilevskaya <antikevkun@gmail.com>
2024-12-22 19:42:57 +0300

    <feat>:Add necessary functions for recipe repository and test them

--- RecipeApp/Repositories/Interfaces/IRecipeRepository.cs
+++ RecipeApp/Repositories/Interfaces/IRecipeRepository.cs
@@ -6,6 +6,7 @@ public interface IRecipeRepository
 {
     public Task<Recipe> GetByIdAsync(int id, CancellationToken cancellationToken = default);
     public Task CreateAsync(Recipe entity, CancellationToken cancellationToken = default);
+    public Task CreateWithIngredientsAsync(Recipe entity, List<Ingredient> ingredients, CancellationToken cancellationToken = default);
     public Task<bool> UpdateAsync(Recipe entity, CancellationToken cancellationToken = default);
     public Task<bool> DeleteAsync(Recipe entity, CancellationToken cancellationToken = default);
     public Task<Recipe> FirstOrDefaultAsync(Expression<Func<Recipe, bool>> filters, CancellationToken cancellationToken = default);
@@ -13,4 +14,5 @@ public interface IRecipeRepository
     public Task<IEnumerable<Recipe>> GetAllAsync(CancellationToken cancellationToken = default);
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     public Task<List<Recipe>> GetRecipesByIngredientSubsetAsync(List<string> ingredientNames);
+    public Task<List<Ingredient>> GetIngredientsByRecipeIdAsync(int recipeId, CancellationToken cancellationToken = default);
 }
