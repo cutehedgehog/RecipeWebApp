0cf420fc3f1830333a9ba8977bd251d7d0d374e6
cutehedgehog <92777569+cutehedgehog@users.noreply.github.com>
2024-12-22 15:10:50 +0300

    <feat>: stock-management
    Complete stock management functionality

--- RecipeApp/Pages/Index.cshtml.cs
+++ RecipeApp/Pages/Index.cshtml.cs
@@ -1,14 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RecipeApp.Models;
 using RecipeApp.Repositories.Interfaces;
 
 namespace RecipeApp.Pages;
+[IgnoreAntiforgeryToken]
 public class IndexModel : PageModel
 {
     private readonly IIngredientRepository _repository;
+    private const string SelectedItemsSessionKey = "SelectedItems";
+
 
     public IndexModel(IIngredientRepository repository)
     {
@@ -17,23 +21,50 @@ public class IndexModel : PageModel
 
     public List<Ingredient> AutoCompleteItems { get; set; } = new List<Ingredient>();
     [BindProperty]
-    public string SearchTerm { get; set; } = string.Empty;
-    [BindProperty]
     public List<string> SelectedItems { get; set; } = new List<string>();
 
     public async Task<IActionResult> OnGet()
     {
+        var sessionData = HttpContext.Session.GetString(SelectedItemsSessionKey);
+        SelectedItems = string.IsNullOrEmpty(sessionData)
+            ? new List<string>()
+            : sessionData.Split(',').ToList();
         AutoCompleteItems = (await _repository.GetAllAsync()).ToList();
         return Page();
     }
 
-    public IActionResult OnPost()
+    public IActionResult OnPostUpdateSelected(string sItem)
     {
-        if (!string.IsNullOrEmpty(SearchTerm) && AutoCompleteItems.Select(x => x.Name).Contains(SearchTerm))
+        
+        if (!string.IsNullOrEmpty(sItem))
         {
-            SelectedItems.Add(SearchTerm);
-        }
+            var sessionData = HttpContext.Session.GetString(SelectedItemsSessionKey);
+            SelectedItems = string.IsNullOrEmpty(sessionData)
+                ? new List<string>()
+                : sessionData.Split(',').ToList();
 
-        return Page();
+            if (!SelectedItems.Contains(sItem, StringComparer.OrdinalIgnoreCase))
+            {
+                SelectedItems.Add(sItem);
+                HttpContext.Session.SetString(SelectedItemsSessionKey, string.Join(",", SelectedItems));
+            }
+        }
+        return new OkResult();
+
+
+    }
+    public IActionResult OnPostRemoveSelected(string sItem)
+    {
+        if (!string.IsNullOrEmpty(sItem))
+        {
+            var sessionData = HttpContext.Session.GetString(SelectedItemsSessionKey);
+            SelectedItems = string.IsNullOrEmpty(sessionData)
+                ? new List<string>()
+                : sessionData.Split(',').ToList();
+
+            SelectedItems.RemoveAll(item => item.Equals(sItem, StringComparison.OrdinalIgnoreCase));
+            HttpContext.Session.SetString(SelectedItemsSessionKey, string.Join(",", SelectedItems));
+        }
+        return new OkResult();
     }
 }
--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -6,25 +6,21 @@ using RecipeApp.Repositories.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddHttpClient();
 builder.Services.AddRazorPages();
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
 builder.Services.AddTransient<IIngredientRepository, IngredientRepository>();
 
 var app = builder.Build();
 await DbInitializer.SeedData(app);
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
@@ -32,11 +28,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 app.MapRazorPages();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
